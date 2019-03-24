ed85acb12b89d43d25627cc65fb43a55128b5103
Alexey <krugombeda@gmail.com>
2019-03-24 18:22:37 +0700

    change generating random array

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,33 @@
 function randomArray(total) {
-  const arr = [];
+  let arr = [];
 
   for (let i = 0; i <= total; i++) {
-    arr.push(getRandomInt(0, total));
+    arr.push(i);
   }
+
+  arr = shuffle(arr, arr.length - 1);
 
   return arr;
 }
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function shuffle(arr, lastIndex) {
+  if (arr.length === 1 || lastIndex === 0) {
+    return arr;
+  } else {
+    let randomIndex = getRandomInt(0, lastIndex);
+    let tempValue;
+
+    tempValue = arr[lastIndex];
+    arr[lastIndex] = arr[randomIndex];
+    arr[randomIndex] = tempValue;
+    lastIndex -= 1;
+
+    return shuffle(arr, lastIndex);
+  }
 }
 
 module.exports = {
